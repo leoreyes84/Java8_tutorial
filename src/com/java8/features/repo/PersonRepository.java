5caeaed62dff7b7796576e15ba45645811527b29
Leonardo Reyes <Leonardo.ReyesRivero@wnco.com>
2020-04-27 20:02:40 -0500

    Repos

---
+++ src/com/java8/features/repo/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.java8.features.repo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PersonRepository {
+	
+	public static List<Person> getAllPersons(){
+		Person p1 = new Person("Jhon", 165, 2000, "Male", 2, Arrays.asList("Criket", "Swimming"));
+		Person p2 = new Person("Angle", 158, 5000, "Female", 2, Arrays.asList("Driving", "Music"));
+		Person p3 = new Person("Jhon", 140, 5700, "Male", 2, Arrays.asList("Criket", "Driving"));
+		Person p4 = new Person("Jhon", 130, 4900, "Female", 2, Arrays.asList("Criket", "Swimming"));
+		Person p5 = new Person("Jhon", 125, 9000, "Female", 2, Arrays.asList("Footbol", "Swimming"));
+		Person p6 = new Person("Jhon", 110, 9500, "Male", 2, Arrays.asList("Criket", "Swimming"));
+		
+		return Arrays.asList(p1, p2, p3, p4, p5, p6);
+	}
+
+}

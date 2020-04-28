00896847c2be70ae6260d9801439b6bb3900ba81
Leonardo Reyes <Leonardo.ReyesRivero@wnco.com>
2020-04-27 21:01:10 -0500

    Consumer example 3

--- src/com/java8/features/consumer/ConsumerPersonExample.java
+++ src/com/java8/features/consumer/ConsumerPersonExample.java
@@ -1,5 +1,6 @@
 package com.java8.features.consumer;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import com.java8.features.repo.Person;
@@ -7,7 +8,7 @@ import com.java8.features.repo.PersonRepository;
 
 public class ConsumerPersonExample {
 	
-	static Consumer<Person> c1 = (p) -> System.out.println(p);
+	static Consumer<Person> c1 = (p) -> System.err.println(p);
 
 	static Consumer<Person> c2 = (p) -> System.out.println(p.getName().toUpperCase());
 	
@@ -15,11 +16,23 @@ public class ConsumerPersonExample {
 	
 	public static void main(String[] args) {
 		
-		c1.accept(PersonRepository.getPerson());
-		c2.accept(PersonRepository.getPerson());
-		c3.accept(PersonRepository.getPerson());
+//		c1.accept(PersonRepository.getPerson());
+//		c2.accept(PersonRepository.getPerson());
+//		c3.accept(PersonRepository.getPerson());
+//		System.out.println("---------------------");
+//		c1.andThen(c2).andThen(c3).accept(PersonRepository.getPerson());
+		
+		printNamesAndHobbies();
+		
+	}
+	
+	static void printNamesAndHobbies() {
+		List<Person> personList = PersonRepository.getAllPersons();
+		personList.forEach(p->System.out.println(p));
 		System.out.println("---------------------");
-		c1.andThen(c2).andThen(c3).accept(PersonRepository.getPerson());
+		personList.forEach(System.out::println);
+		System.out.println("---------------------");
+		personList.forEach(c1);
 		
 	}
 
--- src/com/java8/features/repo/PersonRepository.java
+++ src/com/java8/features/repo/PersonRepository.java
@@ -7,7 +7,7 @@ public class PersonRepository {
 	
 	public static List<Person> getAllPersons(){
 		Person p1 = new Person("Jhon", 165, 2000, "Male", 2, Arrays.asList("Criket", "Swimming"));
-		Person p2 = new Person("Angle", 158, 5000, "Female", 2, Arrays.asList("Driving", "Music"));
+		Person p2 = new Person("Angel", 158, 5000, "Female", 2, Arrays.asList("Driving", "Music"));
 		Person p3 = new Person("Jhon", 140, 5700, "Male", 2, Arrays.asList("Criket", "Driving"));
 		Person p4 = new Person("Jhon", 130, 4900, "Female", 2, Arrays.asList("Criket", "Swimming"));
 		Person p5 = new Person("Jhon", 125, 9000, "Female", 2, Arrays.asList("Footbol", "Swimming"));

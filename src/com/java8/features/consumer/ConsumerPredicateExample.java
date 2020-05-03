38ccfbca5768db2e07411716666e7f50772c339f
Leonardo Reyes <l.reyes@globant.com>
2020-05-03 18:21:32 -0500

    BiPredicate example

--- src/com/java8/features/consumer/ConsumerPredicateExample.java
+++ src/com/java8/features/consumer/ConsumerPredicateExample.java
@@ -2,6 +2,7 @@ package com.java8.features.consumer;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,6 +13,8 @@ public class ConsumerPredicateExample {
 
 	static Predicate<Person> p1 = p -> p.getHeight() >= 140;
 	static Predicate<Person> p2 = p -> p.getGender().equals("Male");
+	// BiPredicate with the same result
+	static BiPredicate<Integer, String> p3 = (height, gender) -> height >= 140 && gender.equals("Male");
 	static BiConsumer<String, List<String>> hobbiesConsummer = (name, hobbies) -> System.out
 			.println("name: " + name + " hobbies: " + hobbies);
 	static Consumer<Person> personConsummer = per -> {
@@ -19,10 +22,17 @@ public class ConsumerPredicateExample {
 			hobbiesConsummer.accept(per.getName(), per.getHobbies());
 		}
 	};
+	static Consumer<Person> personConsummer2 = per -> {
+		if (p3.test(per.getHeight(), per.getGender())) {
+			hobbiesConsummer.accept(per.getName(), per.getHobbies());
+		}
+	};
 
 	public static void main(String[] args) {
 		List<Person> personList = PersonRepository.getAllPersons();
 		personList.forEach(personConsummer);
+		System.out.println("-----------");
+		personList.forEach(personConsummer2);
 
 	}
 
